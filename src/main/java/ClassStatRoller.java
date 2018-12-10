7387f2a0e058d6b83c81e4f00ecc1ce9ea6c4458
Foqz <fikskamil@gmail.com>
2018-12-10 20:03:48 +0100

    userdialogs for picking a class

--- src/main/java/ClassStatRoller.java
+++ src/main/java/ClassStatRoller.java
@@ -1,6 +1,14 @@
 public class ClassStatRoller {
 
     public static void main(String[] args) {
+        UserDialogs userDialogs = new UserDialogs();
+        UserOptions classChoice = userDialogs.welcomeChoice();
+        String charName = userDialogs.userNameCreator(classChoice);
+
+
+
+
         BaseCharacter baseCharacter = new BaseCharacter();
+
     }
 }
---
+++ src/main/java/UserOptions.java
@@ -0,0 +1,5 @@
+public enum  UserOptions {
+    KNIGHT,
+    MAGE,
+    ROGUE,
+}
